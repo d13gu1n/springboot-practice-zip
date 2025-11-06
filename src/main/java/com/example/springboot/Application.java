16d893ba28ee0b82fe2c3f487ffe0832b80e9623
dacevedom <d.acevedom1993@gmail.com>
2025-11-06 15:59:18 -0300

    First iteration of Springboot Practice App using ZIP generated from Springboot Initializr

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	// TODO: Add springboot-practice-zip repo on GitHub as origin Git repository, create candidate branch and merge with main
+	// TODO: Type Bean commandLineRunner method -> https://spring.io/guides/gs/spring-boot
+
+}
